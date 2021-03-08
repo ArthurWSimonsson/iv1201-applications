d69988d2b3d7b4834ffa601ed1cf7eaa97c19e9b
ArthurWSimonsson <simonsson.arthurw@gmail.com>
2021-03-08 14:13:56 +0100

    fixed error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const data = require("./data/Data");
+const data = require("./data/data");
 const dbController = require("./controllers/DBController");
 const compAPI = require('./routes/appAPI')
 
